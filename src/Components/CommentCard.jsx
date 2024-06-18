c9f17126b92a413ce4aad5601a176de56935e38b
lucMarteau <lucmarteau@gmail.com>
2024-06-18 16:59:18 +0100

    Task 7 increment votes

--- src/Components/CommentCard.jsx
+++ src/Components/CommentCard.jsx
@@ -1,13 +1,31 @@
 import React from "react";
 import "./CommentCard.css";
+import { patchArticleVotes } from "../Utilities/api"; 
+import { useState } from "react";
 
-export default function CommentCard({ comment }) {
+
+export default function CommentCard({ comment, article_id }) {
+  const [votes, setVotes] = useState(comment.votes)
+  const [error, setError] = useState(null)
+
+
+  const handleClick = () => {
+    setVotes((prevVotes) => prevVotes + 1)
+    patchArticleVotes(article_id, 1)
+    .catch((error) => {
+
+      setVotes((prevVotes) => prevVotes - 1)
+      setError(error.message)
+    })
+  }
+
   return (
     <div className="comment-card">
       <p>User name: {comment.author}</p>
       <p>{comment.body}</p>
       <button className="comment-button">Comment</button>
-      <button className="vote-button">Vote</button>
+      <button className="vote-button" onClick={handleClick}>Vote</button>
+      <p>{votes} votes</p>
     </div>
   );
 }
