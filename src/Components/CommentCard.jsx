ed9a52e50706786edfe5999040296af7fe130d60
lucMarteau <lucmarteau@gmail.com>
2024-06-19 10:46:20 +0100

    Task 7 with some styling and an undo vote button.

--- src/Components/CommentCard.jsx
+++ src/Components/CommentCard.jsx
@@ -4,17 +4,31 @@ import { patchArticleVotes } from "../Utilities/api";
 import { useState } from "react";
 import Alert from '@mui/material/Alert';
 import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 export default function CommentCard({ comment, article_id }) {
   const [votes, setVotes] = useState(comment.votes);
   const [error, setError] = useState(null);
+  const [undo, setUndo] = useState(false);
+
+  const handleUndo = () => {
+    setVotes((currentVotes) => currentVotes - 1);
+    patchArticleVotes(article_id, -1).catch((error) => {
+      setVotes((currentVotes) => currentVotes + 1);
+      setError("An error occured when trying to undo");
+    });
+    setUndo(false);
+  };
 
   const handleClick = () => {
     setVotes((currentVotes) => currentVotes + 1);
     patchArticleVotes(article_id, 1).catch((error) => {
       setVotes((currentVotes) => currentVotes - 1);
-      setError("An error has occured when trying to contact the server to upload your vote");
+      setError(
+        "An error has occured when trying to contact the server to upload your vote"
+      );
     });
+    setUndo(true);
   };
 
   return (
@@ -25,12 +39,27 @@ export default function CommentCard({ comment, article_id }) {
       <button className="vote-button" onClick={handleClick}>
         Vote
       </button>
-      <p>{votes} votes</p>
-      {error && (
-        <Stack sx={{ width: '100%'}} spacing={2}> 
-        <Alert severity="error">{error}</Alert>
+      {undo && (
+        <Stack sx={{ width: "100%" }} spacing={2}>
+          <Alert
+            severity="success"
+            action={
+              <Button color="inherit" size="small" onClick={handleUndo}>
+                UNDO
+              </Button>
+            }
+          >
+            You have succesfully contributed your vote to this article
+          </Alert>
         </Stack>
       )}
+
+      {error && (
+        <Stack sx={{ width: "100%" }} spacing={2}>
+          <Alert severity="error">{error}</Alert>
+        </Stack>
+      )}
+      <p>Votes total: {votes}</p>
     </div>
   );
 }
