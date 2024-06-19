6b6a811db224670bddd87fcf53e860171bfe7523
lucMarteau <lucmarteau@gmail.com>
2024-06-19 09:35:53 +0100

    Changes to Ticket 7 inluding an alert if the server does not respond

--- src/Components/CommentCard.jsx
+++ src/Components/CommentCard.jsx
@@ -2,30 +2,35 @@ import React from "react";
 import "./CommentCard.css";
 import { patchArticleVotes } from "../Utilities/api"; 
 import { useState } from "react";
-
+import Alert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
 
 export default function CommentCard({ comment, article_id }) {
-  const [votes, setVotes] = useState(comment.votes)
-  const [error, setError] = useState(null)
-
+  const [votes, setVotes] = useState(comment.votes);
+  const [error, setError] = useState(null);
 
   const handleClick = () => {
-    setVotes((currentVotes) => currentVotes + 1)
-    patchArticleVotes(article_id, 1)
-    .catch((error) => {
-
-      setVotes((currentVotes) => currentVotes - 1)
-      setError("...Error")
-    })
-  }
+    setVotes((currentVotes) => currentVotes + 1);
+    patchArticleVotes(article_id, 1).catch((error) => {
+      setVotes((currentVotes) => currentVotes - 1);
+      setError("An error has occured when trying to contact the server to upload your vote");
+    });
+  };
 
   return (
     <div className="comment-card">
       <p>User name: {comment.author}</p>
       <p>{comment.body}</p>
       <button className="comment-button">Comment</button>
-      <button className="vote-button" onClick={handleClick}>Vote</button>
+      <button className="vote-button" onClick={handleClick}>
+        Vote
+      </button>
       <p>{votes} votes</p>
+      {error && (
+        <Stack sx={{ width: '100%'}} spacing={2}> 
+        <Alert severity="error">{error}</Alert>
+        </Stack>
+      )}
     </div>
   );
 }
